370b8001e402f41c0c1b19578ab43cc922ff0626
CodeExpertin <mariedesiree@web.de>
2024-01-04 20:57:34 +0100

    comments

--- pages/question/script.js
+++ pages/question/script.js
@@ -1,10 +1,11 @@
-let questionData    //speichert daten zu der Frage
-let questionIndex   //speichert den index für die Frage
+let questionData    //speichert Daten zu der Frage
+let questionIndex   //speichert den Index fuer die Frage
 
 
-let selectedAnswer  //speichert den index für das ausgewählte Element
+let selectedAnswer  //speichert den Index für das ausgeaehlte Element
 let totalNumberOfQuestions
 
+//Funktion wird aufgerufen sobald eine Antwortmöglichketi gewaehlt wird
 function elementClicked(elementId) {
     if (elementId == 0) {
         document.getElementById('firstClickable').style.backgroundColor = 'steelblue'
@@ -37,16 +38,20 @@ function elementClicked(elementId) {
     }
 }
 
+//Funktion wird aufgerufen sobald auf den Button 'Antwort abegben' geklickt wird
 function submitAnswer() {
+    //pruefen, ob ausgewaehlt wurde
     if (!(selectedAnswer == undefined)) {
+        //pruefen, ob ausgewaehlte Antwort korrekt
         if (questionData.answers[selectedAnswer].right == true) {
+            //hochzaehlen von Index fuer die Frage --> naechste Frage
             questionIndex++
+            //pruefen, ob Quiz fertig
             if (questionIndex >= totalNumberOfQuestions) {
                 window.location.href = "../finished/finished.html"
             } else {
                 window.location.href = `./question.html?questionIndex=${questionIndex}`
             }
-
         } else {
             alert('Falsche Antwort, versuche es erneut ;)')
         }
@@ -55,32 +60,37 @@ function submitAnswer() {
     }
 }
 
+//wird aufgerufen sobald Fenster geladen wird
 window.onload = async function () {
     const parameters = new URLSearchParams(window.location.search)
     questionIndex = parameters.get('questionIndex')
+    //await -> 'wartet' solange bis die Funktion beendet ist
     await loadQuestion()
     setQuestions()
     setQuestionCounter()
 }
 
 function setQuestionCounter(){
+    //in Variable wird aktueller Fragenindex reingespeichert, eins hochgezaehlt und anschließend ausgegeben
     let q = Number(questionIndex) + 1
     document.getElementById('counter').innerText = `Frage ${q} von ${totalNumberOfQuestions}`
 }
 
 function setQuestions() {
+    //'Textfeld' in html Dokument wird in Variable gespeichert
     let firstElement = document.getElementById('firstAnswer')
     let secondElement = document.getElementById('secondAnswer')
     let thirdElement = document.getElementById('thirdAnswer')
     let fourthElement = document.getElementById('fourthAnswer')
-
+    //in Variable wird Frage aus json File reingespeichert -> ausgegeben
     firstElement.textContent = questionData.answers[0].text
     secondElement.textContent = questionData.answers[1].text
     thirdElement.textContent = questionData.answers[2].text
     fourthElement.textContent = questionData.answers[3].text
 
-
+    //... fuer Frage in Html 
     let question = document.getElementById('question')
+    //... fuer Ausgabe Frage
     question.textContent = questionData.question
 }
 
@@ -91,14 +101,16 @@ async function loadQuestion() {
         //oeffnet data.json datei
         let response = await fetch(jsonFilePath);
 
-        // Check if the request was successful (status code 200)
+        // checkt ob Anfrage erfolgreich war (status code 200)
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         // Parse the JSON response
-        //wandelt Variable so um, damit man mithilfe dieser Variable 
+        //wandelt Variable so um, damit man mithilfe dieser Variable arbeiten kann -> Umwandlung
         let data = await response.json()
+        
+        //console.log(data);
 
         totalNumberOfQuestions = data.length
 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Funktion wird aufgerufen sobald Knopf betaetigt wird
 function startQuiz(){
     let questionIndex = 0
     window.location.href = `./pages/question/question.html?questionIndex=${questionIndex}`
